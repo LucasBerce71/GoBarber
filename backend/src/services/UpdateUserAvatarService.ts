82797f0da789014daa903fe66f7f283c1ccb7586
LucasBerce71 <lucas.berce@apinteractive.com.br>
2020-09-08 23:17:23 -0300

    feat: Adding avatar option update

--- backend/src/services/UpdateUserAvatarService.ts
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -1,4 +1,8 @@
 import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
 import User from '../models/User';
 
 interface Request {
@@ -7,7 +11,7 @@ interface Request {
 }
 
 class updateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: Request): Promise<void> {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
     const userRepository = getRepository(User);
 
     const user = await userRepository.findOne(user_id);
@@ -15,6 +19,22 @@ class updateUserAvatarService {
     if (!user) {
       throw new Error('Only authenticated users can change avatar.');
     }
+
+    if (user.avatar) {
+      //Deleting a old avatar
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await userRepository.save(user);
+
+    return user;
   }
 }
 
